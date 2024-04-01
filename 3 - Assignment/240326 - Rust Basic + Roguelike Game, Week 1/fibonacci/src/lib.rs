eba3d5ac56f6c026303dbea76f81c972bd319cd2
ekgns33 <ekgns00421@gmail.com>
2024-04-01 21:19:06 +0900

    feat: write codes for fibonacci number solution

--- 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/fibonacci/src/lib.rs
+++ 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/fibonacci/src/lib.rs
@@ -1,11 +1,21 @@
 pub fn fib(n: u32) -> u32 {
     if n <= 2 {
-        // The base case
-        todo!("Implement this");
+        1
     } else {
         // The recursive case
-        todo!("Implement this");
+        return fib(n-1) + fib(n-2);
     }
+}
+
+pub fn fib_iterative(n: u32) -> u32 {
+    let target: usize = n as usize;
+    let mut dp = vec![0; target+1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for x in 2..=target {
+        dp[x] = dp[x-1] + dp[x-2];
+    }
+    dp[target]
 }
 
 #[cfg(test)]
@@ -33,6 +43,18 @@ mod tests {
     #[test]
     fn fib_20() {
         let ret = fib(20);
+        assert_eq!(ret, 6765);
+    }
+
+    #[test]
+    fn fib_iterative_5() {
+        let ret = fib_iterative(5);
+        assert_eq!(ret, 5)
+    }
+
+    #[test]
+    fn fib_iterative_20() {
+        let ret = fib_iterative(20);
         assert_eq!(ret, 6765);
     }
 }
