9f36db1a75271c1aecb989cc2fe5402cf5f31359
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-04-09 18:06:47 +0900

    Add fibonacci case

--- 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/fibonacci/src/lib.rs
+++ 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/fibonacci/src/lib.rs
@@ -1,10 +1,13 @@
 pub fn fib(n: u32) -> u32 {
-    if n <= 2 {
+    return if n <= 2 {
         // The base case
-        todo!("Implement this");
+        if n == 1 || n == 2 {
+            return 1;
+        }
+        0
     } else {
         // The recursive case
-        todo!("Implement this");
+        fib(n - 1) + fib(n - 2)
     }
 }
 
