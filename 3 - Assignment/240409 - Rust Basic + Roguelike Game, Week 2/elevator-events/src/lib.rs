a72cfaf2e7acd381c3eb2152034e4abe48a1aeec
WhitePiano <startoflateness@gmail.com>
2024-05-05 20:35:35 +0900

    feat: elevator-events

--- 3 - Assignment/240409 - Rust Basic + Roguelike Game, Week 2/elevator-events/src/lib.rs
+++ 3 - Assignment/240409 - Rust Basic + Roguelike Game, Week 2/elevator-events/src/lib.rs
@@ -1,7 +1,17 @@
-#[derive(Debug)]
 // An event in the elevator system that the controller must react to.
+#[derive(Debug)]
 enum Event {
-    // TODO: add required variants
+    ButtonPressed(Button),
+    CarArrived(i32),
+    CarDoorOpened,
+    CarDoorClosed,
+}
+
+// Pressed button.
+#[derive(Debug)]
+enum Button {
+    LobbyCall(Direction, i32),
+    CarFloor(i32),
 }
 
 // A direction of travel.
@@ -13,27 +23,27 @@ enum Direction {
 
 // The car has arrived at the given floor.
 fn car_arrived(floor: i32) -> Event {
-    todo!()
+    Event::CarArrived(floor)
 }
 
 // The car doors have opened.
 fn car_door_opened() -> Event {
-    todo!()
+    Event::CarDoorOpened
 }
 
 // The car doors have closed.
 fn car_door_closed() -> Event {
-    todo!()
+    Event::CarDoorClosed
 }
 
 // A directional button was pressed in an elevator lobby on the given floor.
 fn lobby_call_button_pressed(floor: i32, dir: Direction) -> Event {
-    todo!()
+    Event::ButtonPressed(Button::LobbyCall(dir, floor))
 }
 
 // A floor button was pressed in the elevator car.
 fn car_floor_button_pressed(floor: i32) -> Event {
-    todo!()
+    Event::ButtonPressed(Button::CarFloor(floor))
 }
 
 #[cfg(test)]
