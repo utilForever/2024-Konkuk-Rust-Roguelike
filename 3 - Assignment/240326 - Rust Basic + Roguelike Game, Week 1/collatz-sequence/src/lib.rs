2723f4d59bccbad145a91f7512afc780d1af4cbd
WhitePiano <startoflateness@gmail.com>
2024-05-05 23:13:22 +0900

    refactor: simpler assignments

--- 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/collatz-sequence/src/lib.rs
+++ 3 - Assignment/240326 - Rust Basic + Roguelike Game, Week 1/collatz-sequence/src/lib.rs
@@ -2,14 +2,10 @@
 pub fn collatz_length(mut n: i32) -> u32 {
     let mut length = 1;
     while n != 1 {
-        if n % 2 == 0 {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
-        }
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
         length += 1;
     }
-    
+
     return length;
 }
 
