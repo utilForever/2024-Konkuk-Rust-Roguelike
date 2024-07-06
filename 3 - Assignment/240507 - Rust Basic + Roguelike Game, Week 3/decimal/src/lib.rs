c341397588255006559694f5ef4e50e25e0fbc49
friend1226 <friendcha7749@gmail.com>
2024-07-07 01:12:39 +0900

    solve: 3 - decimal

--- 3 - Assignment/240507 - Rust Basic + Roguelike Game, Week 3/decimal/src/lib.rs
+++ 3 - Assignment/240507 - Rust Basic + Roguelike Game, Week 3/decimal/src/lib.rs
@@ -1,16 +1,18 @@
+use std::ops::{Add, Mul, Sub};
+
 // Type implementing arbitrary-precision decimal arithmetic
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Decimal {
-    sign: bool,
-    integer: Vec<u8>,
-    decimal: Vec<u8>,
+    sign: bool, // true = negative
+    integer: Vec<i8>,
+    decimal: Vec<i8>,
 }
 
 impl Decimal {
     pub fn try_from(input: &str) -> Option<Decimal> {
         let is_sign: bool;
         let mut input_iter = input.chars();
-        let first_character = input.chars().next()?;
+        let first_character = input.chars().next()?; // seek for first character, not consuming iterator
         if first_character == '-' {
             is_sign = true;
             input_iter.next();
@@ -31,53 +33,104 @@ impl Decimal {
         while let Some(c) = input_iter.next() {
             if c == '.' {
                 if is_decimal {
+                    // '.' character should be shown only once
                     return None;
                 }
                 is_decimal = true;
             } else if c.is_ascii_digit() {
                 if is_decimal {
-                    res.decimal.push(c as u8 - 48);
+                    res.decimal.push((c as u8 - 48).try_into().unwrap()); // '0' == 48
                 } else {
-                    res.integer.push(c as u8 - 48);
+                    res.integer.push((c as u8 - 48).try_into().unwrap());
                 }
             } else {
                 return None;
             }
         }
-        if res.integer.len() == 0 {
+        if res.integer.is_empty() {
             res.integer.push(0);
         }
-        if res.decimal.len() == 0 {
+        if res.decimal.is_empty() {
             res.decimal.push(0);
         }
         return Some(res);
     }
 
-    pub fn simplify(&self) -> Decimal {
+    pub fn int_digit(&self) -> usize {
+        self.integer.len()
+    }
+
+    pub fn dec_digit(&self) -> usize {
+        self.decimal.len()
+    }
+
+    pub fn simplify(&self) -> Self {
         let mut res = self.clone();
         let mut first_nonzero_idx: usize = 0;
-        let mut last_nonzero_idx: usize = res.decimal.len() - 1;
-        while first_nonzero_idx < res.integer.len() - 1 && res.integer[first_nonzero_idx] == 0 {
+        let mut last_nonzero_idx: usize = res.dec_digit() - 1;
+        while first_nonzero_idx < res.int_digit() - 1 && res.integer[first_nonzero_idx] == 0 {
             first_nonzero_idx += 1;
         }
         while last_nonzero_idx > 0 && res.decimal[last_nonzero_idx] == 0 {
             last_nonzero_idx -= 1;
         }
-        res.integer = res.integer[last_nonzero_idx..].to_vec();
+        res.integer = res.integer[first_nonzero_idx..].to_vec();
         res.decimal = res.decimal[..=last_nonzero_idx].to_vec();
-        if res.integer.len() == 1
+        if res.int_digit() == 1
             && res.integer[0] == 0
-            && res.decimal.len() == 1
+            && res.dec_digit() == 1
             && res.decimal[0] == 0
         {
-            res.sign = false;
+            res.sign = false; // -0.0 == +0.0
         }
         res
+    }
+
+    pub fn shift_left(&self, value: usize) -> Self {
+        let mut res = self.clone();
+        if res.dec_digit() == 1 && res.decimal[0] == 0 {
+            if !(res.int_digit() == 1 && res.integer[0] == 0) {
+                res.integer.resize(res.int_digit() + value, 0);
+            }
+        } else if res.dec_digit() <= value {
+            let new_int_len = res.int_digit() + value;
+            res.integer.append(&mut res.decimal);
+            res.integer.resize(new_int_len, 0);
+            res.decimal.push(0);
+        } else {
+            res.integer.extend(res.decimal.drain(..value));
+        }
+        if res.int_digit() > 1 && res.integer[0] == 0 {
+            res.integer.drain(..1);
+        }
+        return res;
+    }
+
+    pub fn shift_right(&self, value: usize) -> Self {
+        let mut res = self.clone();
+        if res.int_digit() == 1 && res.integer[0] == 0 {
+            if !(res.dec_digit() == 1 && res.decimal[0] == 0) {
+                res.decimal.splice(..0, std::iter::repeat(0).take(value));
+            }
+        } else if res.int_digit() <= value {
+            let extra_zeros = value - res.int_digit();
+            res.decimal.splice(..0, res.integer);
+            res.decimal
+                .splice(..0, std::iter::repeat(0).take(extra_zeros));
+            res.integer = vec![0];
+        } else {
+            res.decimal
+                .splice(..0, res.integer.drain(res.integer.len() - value..));
+        }
+        if res.dec_digit() > 1 && res.decimal[res.decimal.len() - 1] == 0 {
+            res.decimal.pop();
+        }
+        return res;
     }
 }
 
 impl PartialEq for Decimal {
-    fn eq(&self, other: &Decimal) -> bool {
+    fn eq(&self, other: &Self) -> bool {
         let a = self.simplify();
         let b = other.simplify();
         if a.sign != b.sign || a.integer != b.integer || a.decimal != b.decimal {
@@ -92,33 +145,221 @@ impl PartialOrd for Decimal {
         let a = self.simplify();
         let b = other.simplify();
         if a.sign ^ b.sign {
+            // Both signs are not same
             if a.sign {
-                return Some(std::cmp::Ordering::Greater);
-            } else {
                 return Some(std::cmp::Ordering::Less);
-            }
-        }
-        if a.integer.len() != b.integer.len() {
-            if a.sign {
-                return Some(a.integer.len().cmp(&b.integer.len()));
             } else {
-                return Some(b.integer.len().cmp(&a.integer.len()));
+                return Some(std::cmp::Ordering::Greater);
             }
+            // The cases like -0.0 are illegal since they got simplified.
         }
-        let mut flag = false;
+        // Now both have same sign
+        if a.int_digit() != b.int_digit() {
+            // They have different digits
+            if a.sign {
+                return Some(b.int_digit().cmp(&a.int_digit()));
+            } else {
+                return Some(a.int_digit().cmp(&b.int_digit()));
+            }
+            // The cases like 000123 are illegal since they got simplified.
+        }
+        // Now both have same sign and digits
         let mut a_int = a.integer.clone();
         let mut b_int = b.integer.clone();
         let mut a_int_iter = a_int.iter_mut();
         let mut b_int_iter = b_int.iter_mut();
-        while let Some((na, nb)) = a_int_iter.next().zip(b_int_iter.next()) {}
-        return Some(std::cmp::Ordering::Equal);
+        while let Some((na, nb)) = a_int_iter.next().zip(b_int_iter.next()) {
+            // Check each digit of integer part is same
+            if na == nb {
+                continue;
+            }
+            if a.sign {
+                return Some(nb.cmp(&na));
+            } else {
+                return Some(na.cmp(&nb));
+            }
+        }
+        let mut a_dec = a.decimal.clone();
+        let mut b_dec = b.decimal.clone();
+        let mut a_dec_iter = a_dec.iter_mut();
+        let mut b_dec_iter = b_dec.iter_mut();
+        while let Some((na, nb)) = a_dec_iter.next().zip(b_dec_iter.next()) {
+            // Check each digit of decimal part is same
+            if na == nb {
+                continue;
+            }
+            if a.sign {
+                return Some(nb.cmp(&na));
+            } else {
+                return Some(na.cmp(&nb));
+            }
+        }
+        // If one of them has longer decimal part, that should be greater if positive, less otherwise
+        if a.sign {
+            return Some(b.dec_digit().cmp(&a.dec_digit()));
+        } else {
+            return Some(a.dec_digit().cmp(&b.dec_digit()));
+        }
+        // The cases like 1.234000 are illegal since they got simplified.
+    }
+}
+
+impl Add for Decimal {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        let mut a = self.simplify();
+        let mut b = other.simplify();
+        if a.sign ^ b.sign {
+            // If both have different sign, calculate subtraction instead.
+            if a.sign {
+                a.sign = false;
+                return b - a;
+            } else {
+                b.sign = false;
+                return a - b;
+            }
+        }
+        let new_int_len = a.int_digit().max(b.int_digit()) + 1;
+        let new_dec_len = a.dec_digit().max(b.dec_digit());
+        let a_add_int_len = new_int_len - a.int_digit();
+        let b_add_int_len = new_int_len - b.int_digit();
+        a.integer.resize(new_int_len, 0);
+        a.integer.rotate_right(a_add_int_len);
+        a.decimal.resize(new_dec_len, 0);
+        b.integer.resize(new_int_len, 0);
+        b.integer.rotate_right(b_add_int_len);
+        b.decimal.resize(new_dec_len, 0);
+        let mut carry = 0;
+        for idx in (0..new_dec_len).rev() {
+            a.decimal[idx] += b.decimal[idx] + carry;
+            carry = 0;
+            if a.decimal[idx] > 9 {
+                carry = 1;
+                a.decimal[idx] -= 10;
+            }
+        }
+        for idx in (0..new_int_len).rev() {
+            a.integer[idx] += b.integer[idx] + carry;
+            carry = 0;
+            if a.integer[idx] > 9 {
+                carry = 1;
+                a.integer[idx] -= 10;
+            }
+        }
+        return a.simplify();
+    }
+}
+
+impl Sub for Decimal {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut a = self.simplify();
+        let mut b = rhs.simplify();
+        if a.sign ^ b.sign {
+            b.sign = !b.sign;
+            return a + b;
+        }
+        let new_int_len = a.int_digit().max(b.int_digit());
+        let new_dec_len = a.dec_digit().max(b.dec_digit());
+        let a_add_int_len = new_int_len - a.int_digit();
+        let b_add_int_len = new_int_len - b.int_digit();
+        a.integer
+            .splice(..0, std::iter::repeat(0).take(a_add_int_len));
+        a.decimal.resize(new_dec_len, 0);
+        b.integer
+            .splice(..0, std::iter::repeat(0).take(b_add_int_len));
+        b.decimal.resize(new_dec_len, 0);
+        let mut carry = 0;
+        for idx in (0..new_dec_len).rev() {
+            a.decimal[idx] -= b.decimal[idx] + carry;
+            carry = 0;
+            if a.decimal[idx] < 0 {
+                carry = 1;
+                a.decimal[idx] += 10;
+            }
+        }
+        for idx in (0..new_int_len).rev() {
+            a.integer[idx] -= b.integer[idx] + carry;
+            carry = 0;
+            if a.integer[idx] < 0 {
+                carry = 1;
+                a.integer[idx] += 10;
+            }
+        }
+        if carry == 1 {
+            a.sign = !a.sign;
+            let mut last_nonzero_int_idx = 0;
+            let mut last_nonzero_dec_idx = 0;
+            for idx in (0..new_dec_len).rev() {
+                if a.decimal[idx] > 0 {
+                    last_nonzero_dec_idx = idx + 1;
+                    break;
+                }
+            }
+            if last_nonzero_dec_idx == 0 {
+                for idx in (0..new_int_len).rev() {
+                    if a.integer[idx] > 0 {
+                        last_nonzero_int_idx = idx + 1;
+                        break;
+                    }
+                }
+            }
+            if last_nonzero_dec_idx > 0 {
+                for idx in 0..new_int_len {
+                    a.integer[idx] = 9 - a.integer[idx];
+                }
+                for idx in 0..last_nonzero_dec_idx {
+                    a.decimal[idx] = 9 - a.decimal[idx];
+                }
+                a.decimal[last_nonzero_dec_idx - 1] += 1;
+            } else {
+                for idx in 0..last_nonzero_int_idx {
+                    a.integer[idx] = 9 - a.integer[idx];
+                }
+                a.integer[last_nonzero_int_idx - 1] += 1;
+            }
+        }
+        return a.simplify();
+    }
+}
+
+impl Mul for Decimal {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let a = self.simplify();
+        let b = rhs.simplify();
+        let mut num = a.clone();
+        num.sign = false;
+        let mut res = Decimal {
+            sign: false,
+            integer: vec![0],
+            decimal: vec![0],
+        };
+        num = num.shift_left(b.int_digit() - 1);
+        for idx in 0..b.int_digit() {
+            for _ in 0..b.integer[idx] {
+                res = res + num.clone();
+            }
+            num = num.shift_right(1);
+        }
+        for idx in 0..b.dec_digit() {
+            for _ in 0..b.decimal[idx] {
+                res = res + num.clone();
+            }
+            num = num.shift_right(1);
+        }
+        res.sign = a.sign ^ b.sign;
+        return res.simplify();
     }
 }
 
 // Create a Decimal from a string literal
 //
 // Use only when you _know_ that your value is valid.
-fn decimal(input: &str) -> Decimal {
+pub fn decimal(input: &str) -> Decimal {
     Decimal::try_from(input).expect("That was supposed to be a valid value")
 }
 
